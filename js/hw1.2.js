de877ebfa938dd8372e2f721d9ab8675ea5211b9
Roma <king_roma@ukr.net>
2019-11-28 18:47:30 +0200

    m1

---
+++ js/hw1.2.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const total = 100;
+const ordered = 150;
+
+if (total > ordered) 
+console.log("Заказ оформлен, с вами свяжется менеджер");
+else 
+console.error("На складе недостаточно твоаров!");
